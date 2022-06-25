c8aa1454d877cc3cf151df00cee9a04346038659
nefro85 <1815506+nefro85@users.noreply.github.com>
2022-06-25 23:18:52 +0200

    default checkpoint and save point config tail on flink logs

--- flink/entrypoint.sh
+++ flink/entrypoint.sh
@@ -3,13 +3,20 @@ set -e
 
 args=("$@")
 
+source /opt/flink-config.sh
+
+FLINK_STORAGE=file:/usr/share/flink
+
 info() {
     echo
     echo Apache Flink image by nefro, enjoy...
     echo
     echo ENTRYPOINT ARGS "${args[@]}"
+    echo
+    echo Availabele environment variables:
+    echo
     echo TASK_MANAGER_NUMBER_OF_TASK_SLOTS = ${TASK_MANAGER_NUMBER_OF_TASK_SLOTS:-NOT SPECIFIED}
-    echo FLINK_RUN_JOB = ${FLINK_RUN_JOB}
+    echo FLINK_RUN_JOB = ${FLINK_RUN_JOB:-NOT SPECIFIED}    
     echo
     echo
 }
@@ -42,10 +49,28 @@ flink_ep() {
     exec /opt/flink/flink-entrypoint.sh "${args[@]}"
 }
 
-run_cluster() {
-    source /opt/flink-config.sh
+prepare_flink() {
+
+    echo "# clean setup of flink" > "/opt/flink/conf/flink-conf.yaml"   
+    set_config_option jobmanager.rpc.port: 6123
+    set_config_option jobmanager.memory.process.size ${JOBMANAGER_MEMORY_PROCESS_SIZE:-1600m} 
+    set_config_option taskmanager.memory.process.size ${TASKMANAGER_MEMORY_PROCESS_SIZE:-1728m} 
+    set_config_option parallelism.default ${PARALLELISM_DEFAULT:-1}
+    set_config_option jobmanager.execution.failover-strategy ${JOBMANAGER_EXECUTION_FAILOVER_STRATEGY:-region}
+
+    set_config_option state.backend rocksdb
+    set_config_option state.checkpoints.dir ${FLINK_STORAGE}/checkpoints
+    set_config_option state.savepoints.dir ${FLINK_STORAGE}/savepoints
+    set_config_option state.backend.incremental true
+    set_config_option state.backend.rocksdb.timer-service.factory rocksdb
+
     copy_plugins_if_required
+
     prepare_configuration
+}
+
+run_cluster() {
+    prepare_flink
 
     echo Starting standalone Flink Cluster
     start-cluster.sh
@@ -54,28 +79,17 @@ run_cluster() {
 }
 
 run_ha_cluster() {
-    echo "# clean setup of flink" > "/opt/flink/conf/flink-conf.yaml"
-    source /opt/flink-config.sh
-
-    set_config_option jobmanager.rpc.port: 6123
-    set_config_option jobmanager.memory.process.size 1600m
-    set_config_option taskmanager.memory.process.size 1728m
-    set_config_option parallelism.default 1
-    set_config_option jobmanager.execution.failover-strategy region
-
-
-    copy_plugins_if_required
-    prepare_configuration
+    prepare_flink
 
     zkServer.sh start    
      
     set_config_option high-availability zookeeper
     set_config_option high-availability.zookeeper.quorum localhost:2181
     set_config_option high-availability.zookeeper.path.root /flink
-    set_config_option high-availability.cluster-id /nefro-cluster
+    set_config_option high-availability.cluster-id ${FLINK_HA_CLUSTER_ID:-/nefro-cluster}
     set_config_option high-availability.storageDir file:///usr/share/flink/ha/recovery    
 
-    echo Starting standalone Flink Cluster
+    echo Starting HA Flink Cluster...
     start-cluster.sh
     run_flink_job
     do_forever
@@ -89,7 +103,19 @@ run_flink_job() {
     flink run -d "$FLINK_RUN_JOB"
 }
 
+show_logs() {
+    tail -n 1000 -f /opt/flink/log/*
+}
+
 do_forever() {
+    if [[ -n "${NO_FLINK_LOGS}" ]]; then
+      sleep_forever
+    else
+      show_logs
+    fi
+}
+
+sleep_forever() {
     echo going into forever loop
 
     while :
