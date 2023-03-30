fb414653356dad0754501f59a0a88f7d6cb799eb
nefro85 <mariusz.sygnowski@gmail.com>
2023-03-30 19:09:30 +0200

    [REWORK] WIP enabling Kafka Kraft

--- zk-kafka-stack/entrypoint.sh
+++ zk-kafka-stack/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 set -e
 
+args=("$@")
+
 info() {
     echo Kafka Stack Container
     echo
@@ -55,4 +57,46 @@ exec_script() {
     fi
 }
 
-run_main
+get_config_option() {
+  local option=$1  
+  local escaped_option=$(echo ${option} | sed -e "s/\./\\\./g")
+
+  grep -vxE '[[:blank:]]*([#;].*)?' "${KAFKA_CFG}" \
+    | grep -E "^${escaped_option}=.*" \
+    | grep -oP "=.{0}\K.*"
+}
+
+init_log_dir() {
+    LOG_DIR=$(get_config_option log.dirs)
+
+    echo "Logdir: ${LOG_DIR}"    
+
+    if [[ ! -e ${LOG_DIR} ]]; then
+        mkdir -p ${LOG_DIR}
+        CLUSTER_UID=$(${KAFKA_HOME}/bin/kafka-storage.sh random-uuid)
+        echo "New Cluster UID: ${CLUSTER_UID}"
+        ${KAFKA_HOME}/bin/kafka-storage.sh format -t ${CLUSTER_UID} -c ${KAFKA_CFG}
+    fi
+}
+
+start_kraft_kafka() {
+    info
+    run_glance
+    exec_script&
+
+    add_default_config
+    envsubst < /opt/kraft-server.tmpl > ${KAFKA_CFG}
+
+    echo "Starting Kafka (Kraft) with configuration: ${KAFKA_CFG}"
+    init_log_dir
+
+    ${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_CFG}
+}
+
+case ${args[0]} in
+  kraft)
+    start_kraft_kafka "${args[@]:1}"
+    ;;
+  *)
+  run_main "${args[@]:1}"
+esac
